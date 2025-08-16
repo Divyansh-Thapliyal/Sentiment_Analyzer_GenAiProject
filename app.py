9f56fdb2ff6b741ef3eebc30b03ea08eb4a16dde
Divyansh Thapliyal <divyanshthapliyalwork007@gmail.com>
2025-08-16 20:26:56 +0530

    Gradio functionality added.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # Use a pipeline as a high-level helper
 from transformers import pipeline
+import gradio as gr
 
 # Set a local directory inside your project
 model_dir = "./model"
@@ -10,4 +11,26 @@ classifier = pipeline(
     model="distilbert/distilbert-base-uncased-finetuned-sst-2-english"
 )
 
-print(classifier("I hate books!"))
+# print(classifier("I hate books!"))
+
+
+
+# Now lets create ui using gradio. So lets create a function.
+
+def sentiment_analysis(text):
+    result = classifier(text)
+    return result
+
+
+demo = gr.Interface(
+    fn=sentiment_analysis,
+    inputs=gr.Textbox(lines=5, label="Input", placeholder="Enter text here..."),
+    outputs=gr.Textbox(lines=5, label="Result",placeholder="Sentiment result will appear here..."),
+    title= "Sentiment Analysis Project",
+    description="This is a simple sentiment analysis project using Hugging Face Transformers and Gradio.",
+    theme="default"
+
+)
+
+# Launch the Gradio app
+demo.launch()
